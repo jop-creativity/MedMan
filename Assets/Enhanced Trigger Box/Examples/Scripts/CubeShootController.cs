5db7845df12166d8e2e1996ad9ae819739186e4b
JOP Creativity <j.otto.pawlicki@gmail.com>
2026-05-05 14:24:25 +0200

    S-06 — Additional editor tools installed (Folder Customizer, Enhanced Trigger Box)

---
+++ Assets/Enhanced Trigger Box/Examples/Scripts/CubeShootController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace EnhancedTriggerbox.Examples
+{
+    public class CubeShootController : MonoBehaviour
+    {
+        public void ShootCubes(float velocity)
+        {
+            foreach (var c in GetComponentsInChildren<CubeShoot>())
+            {
+                c.ShootCube(velocity);
+            }
+        }
+    }
+}
+
+
